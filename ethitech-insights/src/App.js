1d3ef0224017fb2e62f0e63ef523ce07a57cdb4a
Brighton Sibanda <brightonsibanda2025@u.northwestern.edu>
2024-08-12 00:40:47 -0400

    initial commit - setup

--- ethitech-insights/src/App.js
+++ ethitech-insights/src/App.js
@@ -1,25 +1,30 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import Footer from './Footer';
 import HomePage from './HomePage';
 import QuizzesPage from './QuizzesPage';
 import DimensionsPage from './DimensionsPage';
 import AboutPage from './AboutPage';
 import ThoughtCorner from './ThoughtCorner';
 import DiversityPage from './DiversityPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/quizzes" element={<QuizzesPage />} />
-        <Route path="/dimensions" element={<DimensionsPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/thought-corner" element={<ThoughtCorner />} />
-        <Route path="/diversity" element={<DiversityPage />} />
-      </Routes>
+      <NavBar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/quizzes" element={<QuizzesPage />} />
+          <Route path="/dimensions" element={<DimensionsPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/thought-corner" element={<ThoughtCorner />} />
+          <Route path="/diversity" element={<DiversityPage />} />
+        </Routes>
+      </div>
+      <Footer />
     </Router>
   );
 }
---
+++ ethitech-insights/src/Footer.js
@@ -0,0 +1,14 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-center text-white mt-4 py-3">
+      <div className="container">
+        © {new Date().getFullYear()} EthiTech Insights. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
