d50041c156e2cbee6be33106ccf01caaa4463b6a
Brighton Sibanda <brightonsibanda2025@u.northwestern.edu>
2024-08-16 07:47:55 -0400

    homepage order

--- ethitech-insights/src/HomePage.js
+++ ethitech-insights/src/HomePage.js
@@ -11,15 +11,15 @@ const HomePage = () => {
       </p>
       <div className="mt-4 text-center">
         {/* Update navigation using Link components for smoother SPA navigation */}
+        <Link to="/defineethics" className="btn btn-info btn-red btn-lg m-2">What Is Ethics</Link>
         <Link to="/dimensions" className="btn btn-primary btn-lg m-2">Explore Ethics Dimensions</Link>
         <Link to="/quizzes" className="btn btn-secondary btn-lg m-2">Mini Ethics Quizzes</Link>
         <Link to="/thought-corner" className="btn btn-info btn-lg m-2">Thought Corner</Link>
         <Link to="/diversity" className="btn btn-warning btn-lg m-2">Diversity and Technology</Link>
-        <Link to="/about" className="btn btn-success btn-lg m-2">About Us</Link>
-        <Link to="/defineethics" className="btn btn-info btn-red btn-lg m-2">What Is Ethics</Link>
-        <Link to="/casestudies" className="btn btn-warning btn-red btn-lg m-2">Case Studies</Link>
+        <Link to="/casestudies" className="btn btn-info btn-red btn-lg m-2">Case Studies</Link>
         <Link to="/debate" className="btn btn-primary btn-red btn-lg m-2">Ethics Debates</Link>
         <Link to="/simulator" className="btn btn-secondary btn-red btn-lg m-2">Decision Simulator</Link>
+        <Link to="/about" className="btn btn-success btn-lg m-2">About Us</Link>
       </div>
     </div>
   );
